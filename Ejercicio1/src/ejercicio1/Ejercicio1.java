c0c20d93a9931a33c26b0daebc06af259c4a869b
UCIEL23 <132796971+UCIEL23@users.noreply.github.com>
2023-06-15 14:46:10 -0600

    Actualizar Ejercicio1.java

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -1,23 +1,86 @@
-package ejercicio1;
-import java.util.Scanner;
-public class Ejercicio1 {
-    public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
-        int a, b, c, d, e, f, g;
-        //c=suma, d=division, e=resta, f=multiplicación, g=modulo
-        System.out.println("Dame un numero: ");
-        a =leer.nextInt();
-        System.out.println("Dame un numero: ");
-        b =leer.nextInt();
-        c = a + b;
-        System.out.println("La suma es: "+c);
-        d= a/b;
-        System.out.println("La division es: "+d);
-        e = a - b;
-        System.out.println("La resta es: "+e);
-        f = a * b;
-        System.out.println("La multiplicacion es: "+f);
-        g = a % b;
-        System.out.println("El modulo es: "+g);
-    }
-}
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license 
+  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template 
+  */ 
+ package persona; 
+  
+ /** 
+  * 
+  * @author yair 
+  */ 
+ public class Persona { 
+  
+     /** 
+      * @param args the command line arguments 
+      */ 
+     public static void main(String[] args) { 
+         // TODO code application logic here 
+     protected String nombre; 
+     protected String materno; 
+     protected String paterno; 
+     protected int edad; 
+     protected float estatura; 
+     protected float peso; 
+  
+     public Persona(String nombre, String materno, String paterno, int edad, float estatura, float peso) { 
+         this.nombre = nombre; 
+         this.materno = materno; 
+         this.paterno = paterno; 
+         this.edad = edad; 
+         this.estatura = estatura; 
+         this.peso = peso; 
+     } 
+  
+     public String getNombre() { 
+         return nombre; 
+     } 
+  
+     public void setNombre(String nombre) { 
+         this.nombre = nombre; 
+     } 
+  
+     public String getMaterno() { 
+         return materno; 
+     } 
+  
+     public void setMaterno(String materno) { 
+         this.materno = materno; 
+     } 
+  
+     public String getPaterno() { 
+         return paterno; 
+     } 
+  
+     public void setPaterno(String paterno) { 
+         this.paterno = paterno; 
+     } 
+  
+     public int getEdad() { 
+         return edad; 
+     } 
+  
+     public void setEdad(int edad) { 
+         this.edad = edad; 
+     } 
+  
+     public float getEstatura() { 
+         return estatura; 
+     } 
+  
+     public void setEstatura(float estatura) { 
+         this.estatura = estatura; 
+     } 
+  
+     public float getPeso() { 
+         return peso; 
+     } 
+  
+     public void setPeso(float peso) { 
+         this.peso = peso; 
+     } 
+  
+     public String imprimirDatos(){ 
+  
+         return "datos desde la clase persona: " + nombre + " " + materno + " " + paterno; 
+     } 
+  
+     }
